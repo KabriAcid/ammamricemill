d34222ff6b602a487e8ebe2b17e72a9cc5144030
KabriAcid <kabriacid01@gmail.com>
2025-09-29 18:10:38 +0100

    Refactor Layout and Sidebar components for improved structure and readability

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,29 +1,20 @@
-import React from 'react';
-import { Navbar } from './Navbar';
-import { Sidebar } from './Sidebar';
-import { useUI } from '../contexts/UIContext';
+import React from "react";
+import { Navbar } from "./Navbar";
+import { Sidebar } from "./Sidebar";
+import { useUI } from "../contexts/UIContext";
 
 interface LayoutProps {
   children: React.ReactNode;
 }
 
 export const Layout: React.FC<LayoutProps> = ({ children }) => {
-  const { sidebarOpen } = useUI();
-
   return (
     <div className="min-h-screen bg-gray-50">
-      <Navbar />
-      <div className="flex pt-16">
-        <Sidebar />
-        <main 
-          className={`
-            flex-1 transition-all duration-300 ease-in-out
-            ${sidebarOpen ? 'lg:ml-80' : 'lg:ml-80'}
-          `}
-        >
-          <div className="p-6">
-            {children}
-          </div>
+      <Sidebar />
+      <div className="lg:ml-80 flex flex-col min-h-screen">
+        <Navbar />
+        <main className="flex-1">
+          <div className="p-6">{children}</div>
         </main>
       </div>
     </div>
